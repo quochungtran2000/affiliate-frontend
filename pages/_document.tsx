8b5d59e246fc82a9ce4ef2cbb20de2dddcb8b506
hung tran <tranquochung6810@gmail.com>
2022-09-30 00:32:44 +0700

    initial material icons

---
+++ pages/_document.tsx
@@ -0,0 +1,41 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const originalRenderPage = ctx.renderPage;
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App: any) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component: React.Component) => Component,
+      });
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
