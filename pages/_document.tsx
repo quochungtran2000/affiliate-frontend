a8c09b1ca8edac10e2c7cd4cad854acd7053d906
hung tran <hungquoc.tran@dxc.com>
2022-10-30 02:44:57 +0700

    add messenger plugin chat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -51,6 +51,38 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <div id="fb-root"></div>
+
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "100427699549445");
+            chatbox.setAttribute("attribution", "biz_inbox");`,
+          }}
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v15.0'
+                });
+              };
+        
+              (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));`,
+          }}
+        ></script>
       </Html>
     );
   }
